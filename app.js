f9dfb5944a7aead603f67b71dceaa2dcb7b6d49b
Rohit Kalkur <rohit.kalkur@gmail.com>
2013-12-14 21:32:10 -0500

    fleshing out various Angular components

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 
 app.get('/new', game.new);
-app.post('/:id', game.save);
+app.post('/save/:id', game.save);
 app.get('/:id', game.detail);
 
 http.createServer(app).listen(app.get('port'), function(){
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,39 +3,38 @@
 	App Namespace
 
 **********************/
-var connectFourApp = angular.module('connectFourApp', []);
+var connectFourApp = angular.module('connectFourApp', ['ngResource']);
 
 /**********************
 
 	Services
 
 **********************/
-connectFourApp.constant('appContantValues', {
+connectFourApp.constant('appConstantValues', {
     gameStates: {
     	START: 0,
     	INPROGRESS: 1,
     	WINNER: 2,
     	DRAW: 3
     },
-    playerTypes: {
+    playerId: {
     	RED: "red",
     	BLACK: "black"
     }
 });
 
-connectFourApp.factory('GameSlotData', ['appContantValues', function(appContantValues) {
+connectFourApp.factory('GameSlotData', ['appConstantValues', function(appConstantValues) {
 
 		function GameSlotData() {
 			this._selectedPlayer = null;
 		}
 
 		Object.defineProperty(GameSlotData.prototype, "selectedPlayer", {
-			get : function() { 
+			get: function() { 
 				return this._selectedPlayer;
 			},
-            set : function(newPlayerType) {
-            	// TODO: Validate input
-            	this._selectedPlayer = newPlayerType;
+            set: function(newPlayer) {
+            	this._selectedPlayer = newPlayer;
             }
         });
 
@@ -43,26 +42,69 @@ connectFourApp.factory('GameSlotData', ['appContantValues', function(appContantV
 	}
 ]);
 
-connectFourApp.factory('gameBoardData', ['appContantValues', function(appContantValues) {
+connectFourApp.factory('gameBoardData', ['appConstantValues', 'GameSlotData', 'gameStates', function(appConstantValues, GameSlotData, gameStates) {
+		var data;
+
 		return {
-			getData : function() {
+			getData: function() {
 				return data;
+			},
+			reset: function(serverData) {
+				data = _transformIntoGameBoardData(serverData);
+			},
+			determineState: function() {
+				/* TODO */
 			}
+		};
+
+		function _transformIntoGameBoardData(serverData) {
+			return serverData.map(function(column) {
+				return column.map(function(slot) {
+					return new GameSlotData();
+				});
+			})
 		}
 	}
 ]);
 
-connectFourApp.factory('gameStateManager', ['appContantValues', function(appContantValues) {
-		var currentState = appContantValues.gameStates.START;
+connectFourApp.factory('gameStateManager', ['appConstantValues', 'GameBoardResource', 'gameBoardData', function(appConstantValues, GameBoardResource, gameBoardData) {
+		var currentState = appConstantValues.gameStates.START,
+			currentPlayer = null;
 
 		return {
-			getCurrentState : function() {
+			getCurrentState: function() {
 				return currentState;
 			},
-			startNewGame : function() {
-				currentState = 	appContantValues.gameStates.INPROGRESS;
+			getCurrentPlayer: function() {
+				return currentPlayer;
+			},
+			startNewGame: function(startingPlayer) {
+				currentPlayer = startingPlayer;
+				GameBoardResource.create().$promise
+					.then(function(response) {
+						gameBoardData.reset(response.data);
+						currentState = appConstantValues.gameStates.INPROGRESS;
+					});
+			},
+			checkStateAndAdvanceGame: function() {
+				/*
+				 TODO: 
+				 - Persist the GameBoardResource
+				*/
+				currentState = gameBoardData.determineState();
+				if(currentState === appConstantValues.gameStates.INPROGRESS) {
+					_toggleCurrentPlayer();
+				};
 			}
 		};
+
+		function _toggleCurrentPlayer() {
+			if(currentPlayer === appConstantValues.playerId.RED) {
+				currentPlayer = appConstantValues.playerId.BLACK;
+			} else {
+				currentPlayer = appConstantValues.playerId.RED;
+			}
+		}
 	}
 ]);
 
@@ -71,26 +113,34 @@ connectFourApp.factory('gameStateManager', ['appContantValues', function(appCont
 	Resources
 
 **********************/
+connectFourApp.factory('GameBoardResource', ['appConstantValues', '$resource', function(appConstantValues, $resource) {
+		var customResourceActions = { 
+			update: {
+				url: '/save/:id',
+				method: 'POST'
+			},
+			create: {
+				url: '/new',
+				method: 'GET'	
+			}
+		};
+
+		return $resource('/:id', { id:'@id'}, customResourceActions);
+ 	}
+]);
 
 /**********************
 
 	Controllers
 
 **********************/
-connectFourApp.controller('GameBoardCtrl', ['$scope', 'gameStateManager', function($scope, gameStateManager) {
-	$scope.data = [
-		[{}, {}, {}, {}, {}, {}, {}], /* Col. 1 */
-		[{}, {}, {}, {}, {}, {}, {}], /* Col. 2 */
-		[{}, {}, {}, {}, {}, {}, {}], /* Col. 3 */
-		[{}, {}, {}, {}, {}, {}, {}], /* Col. 4 */
-		[{}, {}, {}, {}, {}, {}, {}], /* Col. 5 */
-		[{}, {}, {}, {}, {}, {}, {}], /* Col. 6 */
-		[{}, {}, {}, {}, {}, {}, {}]  /* Col. 7 */
-	];
+connectFourApp.controller('GameBoardCtrl', ['$scope', 'gameStateManager', 'gameBoardData', function($scope, gameStateManager, gameBoardData) {
+	$scope.data = gameBoardData.getData();
 
 	$scope.selectSlot = function(clickedSlot) {
-		var getCurrentPlayerType = gameStateManager.getCurrentPlayer;
-		slot.selectedPlayer = getCurrentPlayerType;
+		var getCurrentPlayer = gameStateManager.getCurrentPlayer();
+		clickedSlot.selectedPlayer = getCurrentPlayer;
+		gameStateManager.checkStateAndAdvanceGame();
 	}
 
 	$scope.isSlotSelected = function(slot) {
@@ -102,12 +152,15 @@ connectFourApp.controller('GameBoardCtrl', ['$scope', 'gameStateManager', functi
 	}
 }]);
 
-connectFourApp.controller('GameMenusCtrl', ['$scope', 'gameStateManager', 'appContantValues', function($scope, gameStateManager, appContantValues) {
-	$scope.startingPlayer = appContantValues.playerTypes.RED;
-
-	$scope.$watch(gameStateManager.getCurrentState, function(newState) {
-		if(newState === appContantValues.gameStates.START) {
-
-		}
-	});
+connectFourApp.controller('GameMenusSectionCtrl', ['$scope', function($scope) {
+	console.log("launching GameMenusSectionCtrl")
 }]);
+
+connectFourApp.controller('StartMenuCtrl', ['$scope', 'gameStateManager', 'appConstantValues', function($scope, gameStateManager, appConstantValues) {
+		$scope.startingPlayer = appConstantValues.playerId.RED;
+
+		$scope.start = function() {
+			gameStateManager.startNewGame($scope.startingPlayer);
+		}
+	}
+]);
