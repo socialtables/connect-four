1c007043e79ca6b9f3ee9366820c0f784a7bd286
Rami Chowdhury <rami.chowdhury@gmail.com>
2014-01-08 04:50:03 -0500

    Add application logic

---
+++ public/js/connect-four/bindings.js
@@ -0,0 +1,45 @@
+/**
+ * Custom bindings to KnockoutJS to make the Connect Four game better / easier
+ * to build.
+ */
+
+/**
+ * Fade elements in and out of visibility when bound data changes.
+ * 
+ * Improve on the starkness of the `visible` binding. Based on the `slideVisible`
+ * example in the Knockout docs.
+ */
+ko.bindingHandlers.fadeVisible = {
+    // if the item becomes visible / invisible, slide it in and out of view
+    // using jQuery
+    init: function(element, valueAccessor) {
+        var value = ko.unwrap(valueAccessor());
+        $(element).toggle(value);
+    },
+    update: function(element, valueAccessor) {
+        var value = ko.unwrap(valueAccessor());
+
+        if (value) {
+            $(element).fadeIn("fast");
+        } else {
+            $(element).fadeOut("fast");
+        }
+    }
+};
+
+/**
+ * Provide row and column class labels for individual cells in the Connect
+ * Four grid.
+ * 
+ * Potentially useful for things like highlighting a row or column, or animating
+ * some aspect of gameplay (like dropping a piece in).
+ */
+ko.bindingHandlers.rowColumnClasses = {
+    init: function (element, valueAccessor) {
+        var cell = ko.unwrap(valueAccessor()),
+            col = cell.x,
+            row = cell.y;
+        ko.utils.toggleDomNodeCssClass(element, "row-" + row.toString(), true);
+        ko.utils.toggleDomNodeCssClass(element, "col-" + col.toString(), true);
+    }
+};
---
+++ public/js/connect-four/utils.js
@@ -0,0 +1,102 @@
+/**
+ * Reusable utility functions from Connect Four.
+ * 
+ * (Reusable largely in theory ;-))
+ */
+
+/**
+ * Determine if a row of the given matrix contains a winning 'streak', i.e.
+ * 4 or more of the same value (indicating, in this case, the same player's
+ * pieces) in a row.
+ */
+function containsStreakOfValues(matrix) {
+    var streak = null,
+        streakOf = null,
+        current = null;
+    for (var i = 0; i < matrix.length; i++) {
+        streak = 0;
+        // Iterate backwards since in the Connect Four grid 0,0 is the *top*
+        // left and we want to try from the bottom up
+        for (var j = (matrix[i].length - 1); j >= 0; j--) {
+            current = matrix[i][j];
+            if ((current === null) || (streakOf && (current != streakOf))) {
+                // If the cell is empty or doesn't match the current streak, 
+                // we have to give up
+                streak = 0; 
+                streakOf = null;
+                continue;
+            } else if (!streakOf) {
+                // Perhaps start a new streak?
+                streakOf = current;
+            }
+            if (current == streakOf) {
+                // Increment the current streak, if we've found a corresponding
+                // value.
+                streak++;
+            }
+            if (streak == 4) {
+                // That's a win!
+                return true;
+            }
+        }
+    }
+    // We've tried every row -- no luck.
+    return false;
+}
+
+/**
+ * Given a starting point and a grid, search the diagonals 'up' the virtual
+ * game space to see if a streak of 4 or more values exists.
+ * 
+ * NB: It should be safe to limit the search to 'up' the virtual game space,
+ * since we start at the bottom and so should catch the first item of any 
+ * diagonal run before we see the others.
+ */
+function searchDiagonally(x, y, grid) {
+    var current = grid[x][y],
+        iter_x = x,
+        iter_y = y,
+        max_x = grid.length - 1,
+        streak = 0;
+
+    // First, search decreasing in both indices -- upwards and to the left,
+    // in the virtual game space. 
+    while ((iter_x >= 0) && (iter_y >= 0)) {
+        if (grid[iter_x][iter_y] != current) {
+            // The streak's been broken. Give up.
+            break;
+        }
+        // Optimistic -- keep going.
+        streak++;
+        if (streak == 4) {
+            // Winning condition reached!
+            return true;
+        }
+        // Now increment and continue the search
+        iter_x--;
+        iter_y--;
+    }
+    // If we haven't found a match, try again in the other diagonal direction.
+    // Reset the counters first.
+    iter_x = x;
+    iter_y = y;
+    streak = 0;
+    // This is upwards and to the right, in the virtual space.
+    while ((iter_x <= max_x) && (iter_y >= 0)) {
+        if (grid[iter_x][iter_y] != current) {
+            // Streak broken. Give up.
+            break;
+        }
+        // Optimistic...
+        streak++;
+        if (streak == 4) {
+            // Winning condition reached!
+            return true;
+        }
+        // Increment and continue the search.
+        iter_x++;
+        iter_y--;
+    }
+    // We've tried everything, no luck.
+    return false;    
+}
