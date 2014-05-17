a98ae4e705a8e5ef4a2db66a65df935f83e8f59a
njbennett <nataliejbennett@Natalies-MacBook-Pro.local>
2014-05-17 14:06:01 -0400

    Added validate route
    Functionally complete, still needs some refactoring/tweaking.

--- routes/game.js
+++ routes/game.js
@@ -37,4 +37,52 @@ exports.new = function(req, res){
 
 	res.json({id:id, data:data});
 
+
+}
+
+exports.validate = function(req, res){
+	var board = req.body;
+	var valid = true;
+	var mustHavePiece = false;
+	var player1count = 0;
+	var player2count = 0;
+	var difference;
+
+	board.forEach(function(row) {
+		row.forEach(function(space) {
+			if(mustHavePiece == true && space == null){
+				valid = false
+			}
+
+			if(space != null){
+				mustHavePiece = true
+			}
+		})
+		mustHavePiece = false
+	})
+
+	board.forEach(function(row) {
+		row.forEach(function(space) {
+			if(space == 1){
+				player1count++
+			} else if(space == 2)
+				player2count++
+		})
+	})
+
+	difference = player1count - player2count
+
+	if(difference > 1 || difference < -1){
+		valid = false
+	}
+
+	if(valid == true){
+		res.json(200, {"msg": "Board is valid!"})
+	} else{
+		res.json(200, {"msg": "This board is messed up!"})
+	}
+
+
+
+
 }
